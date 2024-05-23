7e733d5ba266443119318389e0de2bd75dea2da2
Francesco <119010269+FrankOfTheScience@users.noreply.github.com>
2024-05-23 09:59:00 +0200

    Update sensor_controller.ino

--- ArduinoController/sensor_controller.ino
+++ ArduinoController/sensor_controller.ino
@@ -1,16 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <Ticker.h>
 
 const char* ssid = "WIFI_SSID";
 const char* password = "WIFI_PASSWORD";
+const char* iftttWebhookURL = "IFTTT_WEBHOOK_URL ";
 const int sensorPin = 2;
 ESP8266WebServer server(80);
+Ticker timer;
+bool sensorActive = false;
 
 void setup(){
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
@@ -29,18 +33,49 @@ void setup(){
 
 void loop(){
   server.handleClient();
+
+  if (sensorActive) {
+      if (digitalRead(sensorPin) == HIGH){
+        sendIFTTTEvent();
+        handleDeactivate();
+        delay(3600000);
+        handleActivate();
+      }
 }
 
 void handleRoot(){
   server.send(200, "text/plain", "Arduino with ESP8266 module is up and running.";
 }
 
-void handleActivate(){
+void handleActivate() {
   digitalWrite(sensorPin, HIGH);
+  sensorActive = true;
   server.send(200, "text/plain", "Sensor activated");
 }
 
-void handleDeactivate(){
+void handleDeactivate() {
   digitalWrite(sensorPin, LOW);
+  sensorActive = false;
   server.send(200, "text/plain", "Sensor deactivated");
+}
+
+
+void sendIFTTTEvent() {
+  HTTPClient http;
+  
+  http.begin(iftttWebhookURL);
+  http.addHeader("Content-Type", "application/json");
+  
+  int httpResponseCode = http.POST("{\"value1\":\"Alert! Motion detected\"}");
+  
+  if (httpResponseCode > 0) {
+    Serial.print("IFTTT request sent, response code: ");
+    Serial.println(httpResponseCode);
+  }
+  else {
+    Serial.print("Error sending IFTTT request, error code: ");
+    Serial.println(httpResponseCode);
+  }
+  
+  http.end();
 }
